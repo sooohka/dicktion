69c094d92cda20bdc462e76e46ea5ff3b964c8b4
강수호(sooohka) <shkorea1004@gmail.com>
2022-04-04 17:37:24 +0900

    feat: createEl함수 작성, navbar 렌더로직만 작성

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,22 @@
-import Counter from "./components/counter.js";
+import Navbar from "./components/navbar.js";
 
 class App {
   $App: HTMLDivElement;
-
-  $body: HTMLElement;
-
-  state: { count: number };
-
-  $counter: any;
+  $Navbar: Navbar;
 
   constructor() {
     this.$App = document.createElement("div");
-    this.$body = document.body;
     this.$App.className = "root";
-    this.$body.appendChild(this.$App);
-    this.state = {
-      count: 0,
-    };
-    this.$counter = new Counter(this.state.count);
-    this.render();
+    this.$Navbar = new Navbar();
   }
 
-  render() {
-    this.$counter.attach(this.$App);
+  render($parent: HTMLElement) {
+    this.$Navbar.render(this.$App);
+    $parent.appendChild(this.$App);
   }
 }
 
 window.onload = () => {
-  // eslint-disable-next-line no-new
-  new App();
+  const app = new App();
+  app.render(document.body);
 };
